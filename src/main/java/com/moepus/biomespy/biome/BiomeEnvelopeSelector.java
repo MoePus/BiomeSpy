1f7137cdcb6b7d5aa7d308064581eca4bf8ddccd
MoePus <547007249@qq.com>
2026-01-19 20:28:04 +0900

    fix getEnvelope crash

--- src/main/java/com/moepus/biomespy/biome/BiomeEnvelopeSelector.java
+++ src/main/java/com/moepus/biomespy/biome/BiomeEnvelopeSelector.java
@@ -55,7 +55,8 @@ public class BiomeEnvelopeSelector {
     }
 
     public BiomeEnvelope getEnvelope(Climate.ParameterList<Holder<Biome>> parameters, int qx, int qy, int qz) {
-        if (TerrablenderCompat.TERRABLENDER_INSTALLED) {
+        if (TerrablenderCompat.TERRABLENDER_INSTALLED &&
+                ((IParameterListExtendedInfo) parameters).biomeSpy$terraBlenderEnabled()) {
             int uniqueness = TerraBiome.getUniqueness(parameters, qx, qy, qz);
             return envelopeMap.get(uniqueness);
         }
